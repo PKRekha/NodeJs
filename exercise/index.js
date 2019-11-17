f9846bbfa4d464384b695977157d3fe14ee142e0
PKRekha <rekha7.p@gmail.com>
2019-11-17 14:14:28 +0530

    a

---
+++ exercise/index.js
@@ -0,0 +1,12 @@
+// Use `fs` for reading data files and writing the result to `result.out`
+const fs = require('fs');
+
+/**
+ * The main program function.
+ */
+function executeProgram() {
+  // TODO: implement
+}
+
+// Run the program
+executeProgram();
